6a3b9297ec06d17193785b3f01f49e2e7ec2fcbf
Vincent Jacques <vincent@vincent-jacques.net>
2024-11-05 09:43:08 +0100

    Add README ('long_description' required by 'twine check')

---
+++ README.rst
@@ -0,0 +1,1 @@
+An example package for building C/C++ extensions for multiple OSes and architectures
--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@
 import setuptools
 
 
+with open("README.rst") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="jacquev6-multiplatform-example",
     version="0.1.0",
     description="An example package for building C/C++ extensions for multiple OSes and architectures",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     url="https://github.com/jacquev6/jacquev6-multiplatform-example",
     author="Vincent Jacques",
     author_email="vincent@vincent-jacques.net",
